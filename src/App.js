072dd56122d2227679b2c183a699490dee65a0e1
colin1717 <colin1717@mac.com>
2016-12-07 22:06:27 -0600

    removed stock app.css

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
 import React, { Component } from 'react';
-import './App.css';
 import UrlInputForm from './children_components/UrlInputForm';
 import ClientDropdown from './children_components/ClientDropdown';
 import FeatureCard from './children_components/FeatureCard';
