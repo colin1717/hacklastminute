624ce7f08459b422e62059f79f337e8b646348f0
colin1717 <colin1717@mac.com>
2016-12-08 23:42:12 -0600

    working with fake data

--- src/children_components/FeatureCard.js
+++ src/children_components/FeatureCard.js
@@ -5,19 +5,19 @@ class FeatureCard extends Component {
     return (
         <div className='container'>
           <div className="row">
-            <div className="col s12 feature-card">
+            <div className='col s1'></div>
+            <div className="col s10 feature-card">
               <div className="feature-card-header">{ this.props.title }</div>
               <div className="feature-card-content">
                 <div className="feature-card-content-left">
-                  Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+                  Info: { this.props.message }
                 </div>
                 <div className="feature-card-content-right">
-                  <span className='tag stag-prod'>Production/Staging</span>
-                  <span className='tag present'>Present/Missing</span>
-                  <span className='tag instance-name'>Instance Name</span>
+                  <span className='tag present'>Present = {this.props.present.toString()}</span>
                 </div>
               </div>
             </div>
+            <div className='col s1'></div>
           </div>
         </div>
     )
